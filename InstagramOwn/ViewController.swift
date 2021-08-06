3cd28b395b407d6e1fbc4382d0b97b4e60d212f1
Oybek <treyktoriya@gmail.com>
2021-08-06 12:10:28 +0500

    Initial Commit

---
+++ InstagramOwn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramOwn
+//
+//  Created by Oybek To’laboyev on 06/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
